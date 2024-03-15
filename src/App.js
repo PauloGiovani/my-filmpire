9c62b80f5c16e2adce0ae629b74244ece288e5ab
Paulo Giovani <pg_faria@yahoo.com.br>
2024-03-15 19:58:48 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
+// Link 1: https://www.youtube.com/watch?v=b9eMGE7QtTk&t=2318s
+// Link 2: https://filmpire.netlify.app/
+// Tempo: 45min
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>My Filmpire</h1>
+        </div>
+    );
 }
 
 export default App;
